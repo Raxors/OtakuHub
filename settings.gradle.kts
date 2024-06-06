4733673db8593bc29cbbbe9e395188d3b2faf2c1
Лазута Иван Евгеньевич <lazutaie1@pochtabank.ru>
2024-06-07 02:32:19 +0300

    Replace files from template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,31 @@
+rootProject.name = "AnimeXplore"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
-        google()
-        gradlePluginPortal()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
 
-rootProject.name = "AnimeXplore"
-include(":androidApp")
-include(":shared")
+include(":composeApp")
--- shared/src/commonMain/kotlin/com/raxors/animexplore/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.raxors.animexplore
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
